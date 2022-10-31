c847a489d41cde03a3e78105df84094f3a9513d4
Randell Crapy <randell.crapy@gmail.com>
2022-10-31 12:09:47 +0800

    Made use of objects as a way to directly force re-renders in props, even if it's same value.

--- example/src/App.js
+++ example/src/App.js
@@ -8,21 +8,53 @@ import 'react-wallet-chat/dist/index.css'
 const App = () => {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false)
   const [chatAddr, setChatAddr] = useState("0x818cF89054B3A5E03D4677b52982E8319D009194")
+  const [widgetState, setWidgetState] = useState({})
+  //chatAddr={chatAddr} isOpen={isWidgetOpen} setIsOpen={setIsWidgetOpen}
   return (<div>
     {/* <ExampleComponent text="Create React Library Example 😄" /> */}
     <button onClick={() => {
       console.log(`isWidgetOpen: ${isWidgetOpen}`)
-      setIsWidgetOpen(true)
+      // setIsWidgetOpen(true)
+      setWidgetState(
+        {
+           ...widgetState, 
+          isOpen: true
+        }
+      )
     }}>
       Toggle widget overlay
 
     </button>
-    <input type="text" id="lname" name="lname"
+    {/* <input type="text" id="lname" name="lname"
       value={chatAddr}
       onChange={(e) => {
         setChatAddr(e.target.value)
-      }} />
-    <WalletChatWidget chatAddr={chatAddr} setChatAddr={setChatAddr} isOpen={isWidgetOpen} setIsOpen={setIsWidgetOpen} />
+      }} /> */}
+    <button onClick={() => {
+      console.log("Open chat")
+      console.log(`chatAddr: ${chatAddr}`)
+      // setIsWidgetOpen(true)
+      // setChatAddr("anything")
+      // console.log(`chatAddr: ${chatAddr}`)
+      // chatAddr = "anything"
+      // console.log(`chatAddr: ${chatAddr}`)
+      // setChatAddr("0x818cF89054B3A5E03D4677b52982E8319D009194")
+      setWidgetState(
+        {
+           ...widgetState, 
+          chatAddr: "0x818cF89054B3A5E03D4677b52982E8319D009194",
+          isOpen: true
+        }
+      )
+      // console.log(`chatAddr: ${chatAddr}`)
+      console.log(widgetState)
+    }}
+
+    >
+      Open Chat
+
+    </button>
+    <WalletChatWidget widgetState={widgetState} />
   </div>)
 
 
--- src/WalletChat/WalletChat.jsx
+++ src/WalletChat/WalletChat.jsx
@@ -4,21 +4,62 @@ import ButtonOverlay from "../ButtonOverlay/ButtonOverlay";
 import styles from "./WalletChat.module.css";
 import { getCookie } from "../utils";
 
-export default function WalletChatWidget({chatAddr, setChatAddr, isOpen, setIsOpen}) {
-  console.log(`isOpen2: ${isOpen}`)
-  if(isOpen == undefined || setIsOpen == undefined){
-    // const [isOpen, setIsOpen] = useState(isOpen2 != undefined? isOpen2:false);
-    [isOpen, setIsOpen] = useState(false);
+const setExtChatAddr = (setState) => {
+  return (addr) => {
+    setState(undefined)
+    setState(addr)
   }
-  
-  if (chatAddr == undefined || setChatAddr == undefined){
-    [chatAddr, setChatAddr] = useState("");
-  }
+}
+
+const openChat = (addr) => {
+
+}
+// {chatAddr, setChatAddr, isOpen, setIsOpen}
+
+function WalletChatWidget({ widgetState }) {
+  console.log("WalletChatWidget Render")
+  console.log(widgetState)
+  const [isOpen, setIsOpen] = useState(widgetState?.isOpen)
+  const [chatAddr, setChatAddr] = useState(widgetState?.chatAddr)
+  const [hideIframe, setHideIframe] = useState(false);
+  // const [url, setUrl] = useState(process.env.REACT_APP_APP_URL || "https://v1.walletchat.fun")
+  console.log(`WalletChatWidget isOpen: ${isOpen}`)
+  console.log(`WalletChatWidget chatAddr: ${chatAddr}`)
+  // useEffect(()=>{
+  //   if (chatAddr != undefined && chatAddr.length != 0){
+  //     setUrl(`${process.env.REACT_APP_APP_URL || "https://v1.walletchat.fun"}/dm/${chatAddr}`)
+  //   }
+  //   console.log("Cur url: " + url)
+  // },[])
+
+  useEffect(() => {
+    console.log("useEffect widgetState")
+    setIsOpen(widgetState?.isOpen)
+    setChatAddr(widgetState?.chatAddr)
+    setHideIframe(true)
+    setTimeout(() => {
+      setHideIframe(false)
+    }, 100)
+    // let url = process.env.REACT_APP_APP_URL || "https://v1.walletchat.fun" // process.env.REACT_APP_APP_URL || "http://192.168.50.30:3000" || "http://localhost:3000";
+
+    // if (chatAddr != undefined && chatAddr.length != 0){
+    //   setUrl(`${process.env.REACT_APP_APP_URL || "https://v1.walletchat.fun"}/dm/${chatAddr}`)
+    // }
+  }, [
+    widgetState
+  ])
+  // if(isOpen == undefined || setIsOpen == undefined){
+  //   // const [isOpen, setIsOpen] = useState(isOpen2 != undefined? isOpen2:false);
+  //   [isOpen, setIsOpen] = useState(false);
+  // }
+
+  // if (chatAddr == undefined || setChatAddr == undefined){
+  //   [chatAddr, setChatAddr] = useState("");
+  // }
 
   const [numUnread, setNumUnread] = useState(0);
   // const url = process.env.REACT_APP_APP_URL || "http://localhost:3000"  //"https://app.walletchat.fun/"//"app.walletchat.fun" //http://localhost:3000
   let url = process.env.REACT_APP_APP_URL || "https://v1.walletchat.fun" // process.env.REACT_APP_APP_URL || "http://192.168.50.30:3000" || "http://localhost:3000";
-  console.log(`url: ${url}`);
   // if(isOpen2 != undefined)[
   //   setIsOpen(isOpen2)
   // ]
@@ -29,42 +70,46 @@ export default function WalletChatWidget({chatAddr, setChatAddr, isOpen, setIsOp
     setIsOpen(!isOpen);
   };
 
-  if (chatAddr != undefined && chatAddr.length != 0){
+  if (chatAddr != undefined && chatAddr.length != 0) {
     url += `/dm/${chatAddr}`
   }
+  console.log(`url: ${url}`);
+  url = { val: url }
   useEffect(() => {
     window.addEventListener("message", (e) => {
       var data = e.data;
       console.log("RECEIVED message from CHILD TO PARENT");
       console.log(data);
-      if(data["target"] == 'unread_cnt'){
+      if (data["target"] == 'unread_cnt') {
         setNumUnread(data["data"]);
       }
-      
+
     });
   }, []);
-//   setInterval(() => {
-//     let cnt = getCookie("_wallet_chat_msg_cnt");
+  //   setInterval(() => {
+  //     let cnt = getCookie("_wallet_chat_msg_cnt");
 
-//     // setNumUnread(cnt);
-//   }, 3000);
+  //     // setNumUnread(cnt);
+  //   }, 3000);
   return (
     <div id={styles["wallet-chat-widget__container"]}>
       {/* {isOpen && (
                 <iframe id="wallet-chat-widget" src={url}></iframe>
             )} */}
-      <iframe
-        id="wallet-chat-widget"
-        className={styles["wallet-chat-widget"]}
-        style={{
-          height: isOpen ? "" : "0px",
-          width: isOpen ? "" : "0px",
-          minHeight: isOpen ? "" : "0px",
-          minWidth: isOpen ? "" : "0px",
-          // display: isOpen?"block":"none"
-        }}
-        src={url}
-      ></iframe>
+      {!hideIframe &&
+        <iframe
+          id="wallet-chat-widget"
+          className={styles["wallet-chat-widget"]}
+          style={{
+            height: isOpen ? "" : "0px",
+            width: isOpen ? "" : "0px",
+            minHeight: isOpen ? "" : "0px",
+            minWidth: isOpen ? "" : "0px",
+            // display: isOpen?"block":"none"
+          }}
+          src={url.val}
+        ></iframe>
+      }
       <ButtonOverlay
         notiVal={numUnread}
         showNoti={numUnread > 0}
@@ -74,3 +119,6 @@ export default function WalletChatWidget({chatAddr, setChatAddr, isOpen, setIsOp
     </div>
   );
 }
+
+export default
+  WalletChatWidget
